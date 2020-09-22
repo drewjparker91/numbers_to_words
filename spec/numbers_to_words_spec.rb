04de9f387347fac7e2b83e8efcbe7e158a402914
Alex Goodwin <hagoodwi@gmail.com>
2020-09-22 13:06:35 -0700

    add initial tests to project
    Co-Authored-By: Drew Parker <drewjparker91@gmail.com>

---
+++ spec/numbers_to_words_spec.rb
@@ -0,0 +1,16 @@
+
+it("return the corresponding word for a single digit integer") do
+  expect(finalString("1")).to(eq("one"))
+end
+
+# it("return the corrensponding words for a double digit integer") do
+#   expect(finalString("25")).to(eq("twenty five"))
+# end
+
+# it("return the corrensponding words for a triple digit integer") do
+#   expect(finalString("365")).to(eq("three hundred sixty five"))
+# end
+
+# it("return the corrensponding words for a quadruple digit integer") do
+#   expect(finalString("5625")).to(eq("five thousand six hundred twenty five"))
+# end
