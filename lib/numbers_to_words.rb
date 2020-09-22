fc51a4cdb0859b290fac460ff6796d8687e6d19e
Alex Goodwin <hagoodwi@gmail.com>
2020-09-22 16:16:07 -0700

    added 5 passing test and functionality
    Co-Authored-By: Drew Parker <drewjparker91@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,42 +1,121 @@
-def numbers_to_words(number)
-  numbers_to_name = {
-      1000000000000 => "trillion",
-      1000000000 => "billion",
-      1000000 => "million",
-      1000 => "thousand",
-      100 => "hundred",
-      90 => "ninety",
-      80 => "eighty",
-      70 => "seventy",
-      60 => "sixty",
-      50 => "fifty",
-      40 => "forty",
-      30 => "thirty",
-      20 => "twenty",
-      19=>"nineteen",
-      18=>"eighteen",
-      17=>"seventeen", 
-      16=>"sixteen",
-      15=>"fifteen",
-      14=>"fourteen",
-      13=>"thirteen",              
-      12=>"twelve",
-      11 => "eleven",
-      10 => "ten",
-      9 => "nine",
-      8 => "eight",
-      7 => "seven",
-      6 => "six",
-      5 => "five",
-      4 => "four",
-      3 => "three",
-      2 => "two",
-      1 => "one"
-    }
+class Number
+  def initialize(number)
+    @number = number
+    @word = []
+  end
 
-    finalString = ""
-    
-    # if num >0 && < 21, loop returns value of key
-    # if num > 20, we will have to move from tens, hundreds, etc
+def numbers_to_words
+  ones = {
+    "9" => "nine",
+    "8" => "eight",
+    "7" => "seven",
+    "6" => "six",
+    "5" => "five",
+    "4" => "four",
+    "3" => "three",
+    "2" => "two",
+    "1" => "one"
+  }
 
+  teens = {
+    "19" => "nineteen",
+    "18" => "eighteen",
+    "17" => "seventeen", 
+    "16" => "sixteen",
+    "15" => "fifteen",
+    "14" => "fourteen",
+    "13" => "thirteen",              
+    "12" => "twelve",
+    "11" => "eleven",
+  }
+      
+  tens = {
+    "90" => "ninety",
+    "80" => "eighty",
+    "70" => "seventy",
+    "60" => "sixty",
+    "50" => "fifty",
+    "40" => "forty",
+    "30" => "thirty",
+    "20" => "twenty",      
+    "10" => "ten",
+  }
+
+  hundreds = {
+    "100" => "one hundred",
+    "200" => "two hundred",
+    "300" => "three hundred",
+    "400" => "four hundred",
+    "500" => "five hundred",
+    "600" => "six hundred",
+    "700" => "seven hundred",
+    "800" => "eight hundred",
+    "900" => "nine hundred"
+  }
+
+  thousands = {
+    "1000" => "one thousand",
+    "2000" => "two thousand",
+    "3000" => "three thousand",
+    "4000" => "four thousand",
+    "5000" => "five thousand",
+    "6000" => "six thousand",
+    "7000" => "seven thousand",
+    "8000" => "eight thousand",
+    "9000" => "nine thousand"
+  }
+
+    number = @number.chars
+      if(number.length == 4)
+        number_ones = ones.fetch(number[3])
+        number_tens = tens.fetch(number[2].concat("0"))
+        number_hundreds = hundreds.fetch(number[1].concat("00"))
+        number_thousands = thousands.fetch(number[0].concat("000"))
+        word = number_thousands + " " + number_hundreds + " " + number_tens + " " + number_ones
+      elsif (number.length == 3)        
+        number_ones = ones.fetch(number[2])
+        number_tens = tens.fetch(number[1].concat("0"))
+        number_hundreds = hundreds.fetch(number[0].concat("00"))
+        word = number_hundreds + " " + number_tens + " " + number_ones
+      elsif (number.length == 2)
+        if (number.include?("0"))
+          word = tens.fetch(@number)
+        elsif (number.include?("1"))
+          word = teens.fetch(@number)
+        else 
+          number_ones = ones.fetch(number[1])
+          number_tens = tens.fetch(number[0].concat("0"))
+          word = number_tens + " " + number_ones
+        end
+      elsif (number.length == 1)
+        word = ones.fetch(@number)
+      end    
+  end
 end
+
+  # elsif number.length == 2
+  #   tens.fetch(number[2])
+  # elsif number.length == 3
+  #   hundreds.
+ 
+
+# numbers_to_words("25")
+
+
+# else 
+#   ones = ones.fetch(number[1])
+#   tens = tens.fetch(number[0])
+#   number.concat.push
+
+#   43
+
+
+
+
+
+# if number.length = 1
+#   number_to_name.fetch(number)
+
+# elsif number.length = 2
+#   if (number.include?(0))
+#     return number_number_to_name.fetch
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,11 +1,37 @@
+require('rspec')
+require('numbers_to_words')
 
-it("return the corresponding word for a single digit integer") do
-  expect(finalString("1")).to(eq("one"))
+describe("Number#numbers_to_words") do
+  it("return the corresponding word for a single digit integer") do
+    number = Number.new("1")
+    expect(number.numbers_to_words()).to(eq("one"))
+  end
+
+  it("return the corrensponding words for a tens integer") do
+    number = Number.new("20")
+    expect(number.numbers_to_words).to(eq("twenty"))
+  end
+
+  it("return the corrensponding words for a teens integer") do
+    number = Number.new("18")
+    expect(number.numbers_to_words).to(eq("eighteen"))
+  end
+
+  it("return the corrensponding words for a two digit integer") do
+    number = Number.new("88")
+    expect(number.numbers_to_words).to(eq("eighty eight"))
+  end
+
+  it("return the corrensponding words for a three digit integer") do
+    number = Number.new("666")
+    expect(number.numbers_to_words).to(eq("six hundred sixty six"))
+  end
+
+  it("return the corrensponding words for a four digit integer") do
+    number = Number.new("2421")
+    expect(number.numbers_to_words).to(eq("two thousand four hundred twenty one"))
+  end
 end
-
-# it("return the corrensponding words for a double digit integer") do
-#   expect(finalString("25")).to(eq("twenty five"))
-# end
 
 # it("return the corrensponding words for a triple digit integer") do
 #   expect(finalString("365")).to(eq("three hundred sixty five"))
